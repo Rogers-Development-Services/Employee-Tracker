0cccc0e98feab021389e1316a634f362dcfb5c26
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-09-22 14:51:27 -0700

    created functionality for searching for individual employees.

--- app.js
+++ app.js
@@ -22,7 +22,49 @@ const connectionConfig = {
 
 var connection = mysql.createConnection(connectionConfig);
 
-connection.connect(function (err) {
-    if (err) throw err;
+connection.connect(function (error) {
+    if (error) throw error;
     console.log(`connected as id ${connection.threadId}`);
+    viewEmployee ();
 });
+
+function viewDepartment () {};
+
+function viewRole () {};
+
+function viewEmployee () {
+    console.log("Selecting all employees...\n");
+    connection.query("SELECT * FROM employee WHERE ?",
+    [
+        {
+            first_name: "Matthew"
+        }
+    ],
+    function (error, response) {
+        if (error) throw error;
+        // Log all results of the SELECT statement
+        console.log(response);
+        connection.end();
+        });
+    };
+
+function addDepartment () {};
+
+function addRole () {};
+
+function addEmployee () {};
+
+function updateEmployeeRoles () {};
+
+function updateEmployeeManagers () {};
+
+function viewEmployeesByManager () {};
+
+function removeDpartments () {};
+
+function removeRole () {};
+
+function removeEmployee () {};
+
+function viewBudget () {};
+// View the total utilized budget of a department -- ie the combined salaries of all employees in that department
